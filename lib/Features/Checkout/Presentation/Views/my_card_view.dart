bbbad84c0bef46132e96d4f14b6374f430b4ce5b
mohamed <mohamedhassouna88@gmail.com>
2023-10-23 02:46:02 -0700

    create styles

--- lib/Features/Checkout/Presentation/Views/my_card_view.dart
+++ lib/Features/Checkout/Presentation/Views/my_card_view.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:payment_app/core/utils/styles.dart';
 
 class MyCardView extends StatelessWidget {
   const MyCardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'My Cart',
+          textAlign: TextAlign.center,
+          style: Styles.style25,
+        ),
+      ),
+    );
   }
 }
